1c2b9b1dc34643e2303969b28e8183d7baf5cefe
dreamy <dreamy.develop@gmail.com>
2026-01-03 09:18:59 +0500

    update project structure and separate package defenition

---
+++ modules/parts/package.nix
@@ -0,0 +1,16 @@
+{inputs, ...}: {
+  perSystem = {pkgs, ...}: let
+    mylib = import ../../lib {inherit (inputs.nixpkgs) lib;};
+    customNeovim = inputs.nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {
+        inherit mylib;
+      };
+      modules = [
+        ../../modules/neovim
+      ];
+    };
+  in {
+    packages.default = customNeovim.neovim;
+  };
+}
