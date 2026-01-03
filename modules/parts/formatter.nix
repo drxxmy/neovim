cc72882d8e47514b837b3ae0d7e92af83b1484ec
Vladislav <118668125+drxxmy@users.noreply.github.com>
2026-01-03 09:19:29 +0500

    add formatter

---
+++ modules/parts/formatter.nix
@@ -0,0 +1,9 @@
+{inputs, ...}: {
+  imports = [inputs.treefmt-nix.flakeModule];
+  perSystem = {pkgs, ...}: {
+    treefmt = {
+      programs.nixfmt.enable = true;
+      programs.nixfmt.package = pkgs.nixfmt;
+    };
+  };
+}
