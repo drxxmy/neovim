a7fa1fb75d41754898c90a325da640d8c98e52a9
dreamy <dreamy.develop@gmail.com>
2026-02-10 11:31:06 +0500

    feat: add harpoon configuration

---
+++ modules/plugins/harpoon.nix
@@ -0,0 +1,13 @@
+{
+  config.vim.navigation.harpoon = {
+    enable = true;
+    mappings = {
+      file1 = "<leader>na";
+      file2 = "<leader>ns";
+      file3 = "<leader>nd";
+      file4 = "<leader>nf";
+      listMarks = "<C-e>";
+      markFile = "<leader>a";
+    };
+  };
+}
