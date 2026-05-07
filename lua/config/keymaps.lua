6bea6701562e35e30fabe39c8f4a412fc2df6494
dreamy <dreamy.develop@gmail.com>
2026-05-07 14:17:57 +0500

    refactor(keymaps): remove comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,3 @@ local map = vim.keymap.set
 -- Splits
 map("n", "<leader>-", "<cmd>horizontal split<cr>", { desc = "Open horizontal split" })
 map("n", "<leader>|", "<cmd>vertical split<cr>", { desc = "Open vertical split" })
--- Buffers
--- map("n", "<M-h>", "<cmd>bprev<cr>")
--- map("n", "<M-l>", "<cmd>bnext<cr>")
