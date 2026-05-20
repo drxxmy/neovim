e36389c8b0d57bcebb9c96ebcafe5f3b15d1793e
dreamy <dreamy.develop@gmail.com>
2026-05-20 20:32:09 +0500

    feat(term): add keymap to exit terminal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@ local map = vim.keymap.set
 -- Splits
 map("n", "<leader>-", "<cmd>horizontal split<cr>", { desc = "Open horizontal split" })
 map("n", "<leader>|", "<cmd>vertical split<cr>", { desc = "Open vertical split" })
+
+-- Terminal mode
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit Terminal mode" })
