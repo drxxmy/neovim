da2a43fcc8c952e92df46d5c1b2feb739f575259
dreamy <dreamy.develop@gmail.com>
2026-06-01 00:04:14 +0500

    feat(keymaps): add keymap to reload buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 local map = vim.keymap.set
 
+-- Buffers
+map("n", "<leader>br", "<cmd>bufdo e<cr>", { desc = "Reload buffer" })
+
 -- Splits
 map("n", "<leader>-", "<cmd>horizontal split<cr>", { desc = "Open horizontal split" })
 map("n", "<leader>|", "<cmd>vertical split<cr>", { desc = "Open vertical split" })
