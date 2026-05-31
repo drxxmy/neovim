badb89428f00611b178dad528a8e1662dce456da
dreamy <dreamy.develop@gmail.com>
2026-06-01 00:51:33 +0500

    feat(keymaps): add keymap to toggle buffer diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,10 @@ local map = vim.keymap.set
 
 -- Buffers
 map("n", "<leader>br", "<cmd>bufdo e<cr>", { desc = "Reload buffer" })
+map("n", "<leader>bd", function()
+	local bufnr = 0
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+end, { desc = "Toggle buffer diagnostics" })
 
 -- Splits
 map("n", "<leader>-", "<cmd>horizontal split<cr>", { desc = "Open horizontal split" })
