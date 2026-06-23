ced783166c6c6af18907329bfbf3a31fce62af24
dreamy <dreamy.develop@gmail.com>
2026-06-23 16:20:23 +0500

    feat(snacks): add keymaps to view github issues & pull requests

--- lua/lazy/snacks.lua
+++ lua/lazy/snacks.lua
@@ -207,6 +207,35 @@ return {
       end,
       desc = "Toggle Lazygit",
     },
+    -- GitHub
+    {
+      "<leader>gi",
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = "GitHub Issues (Open)",
+    },
+    {
+      "<leader>gI",
+      function()
+        Snacks.picker.gh_issue({ state = "all" })
+      end,
+      desc = "GitHub Issues (All)",
+    },
+    {
+      "<leader>gp",
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = "GitHub Pull Requests (Open)",
+    },
+    {
+      "<leader>gP",
+      function()
+        Snacks.picker.gh_pr({ state = "all" })
+      end,
+      desc = "GitHub Pull Requests (All)",
+    },
     -- Words
     {
       "]]",
